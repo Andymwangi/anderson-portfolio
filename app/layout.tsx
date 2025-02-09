37e1de7e6b91c7be04e05827708b7c51ac57e3c5
Andymwangi <andymwangi3@gmail.com>
2025-02-10 00:57:53 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./Provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Anderson's Portfolio",
+  description: "Modern and intuitive portfolio showcasing Anderson's skills",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,174 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building full stack apps with various technologies",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "/ui.earth.com",
+    },
+    {
+        id: 2,
+        title: "BookWise - University Library Management System",
+        des: "BookWise is a university library management web app that allows users to borrow books and read descriptions and summaries of a vast collection of books.",
+        img: "/bookwise.PNG",
+        iconLists: ["/next.svg", "/postgres.svg", "/redis.svg", "/shadcnui.svg", "tailwindcss.svg"],
+        link: "https://bookwise-libraryapp.vercel.app/",
+      },
+      {
+        id: 3,
+        title: "StoreIt - Digital File Management System",
+        des: "A digital file management system that leverages OCR for document uploading and secure storage.",
+        img: "/p3.svg",
+        iconLists: ["/next.svg", "/appwrite.svg", "/redis.svg", "tailwindcss.svg"],
+        link: "/ui.storeIt.com",
+      },
+      {
+        id: 4,
+        title: "Minty - Banking Web Application",
+        des: "A secure banking web application that facilitates seamless transactions and user authentication.",
+        img: "/Minty.PNG",
+        iconLists: ["/next.svg", "/appwrite.svg", "tailwindcss.svg", "redis.svg"],
+        link: "https://minty-bank-app.vercel.app/",
+      }
+    ];
+    
+    export const testimonials = [
+      {
+        quote:
+          "Anderson's technical expertise and commitment to excellence transformed our vision into reality. His ability to tackle complex problems with innovative solutions is truly commendable.",
+        name: "James Ochieng",
+        title: "CTO of TechWave Solutions",
+      },
+      {
+        quote:
+          "Working with Anderson was a game-changer. His proficiency in backend and full-stack development helped streamline our operations efficiently.",
+        name: "Sarah Njeri",
+        title: "Project Manager at Innovate Hub",
+      },
+      {
+        quote:
+          "Anderson demonstrated exceptional skill and professionalism throughout our collaboration. His ability to integrate secure banking features in Minty exceeded our expectations.",
+        name: "David Kamau",
+        title: "CEO of FinTrust Ltd.",
+      }
+    ];
+    
+    
+    export const workExperience = [
+      {
+        id: 1,
+        title: "Frontend Engineer Intern - Kenya Law",
+        desc: "Assisted in the development of a web-based legal research platform using React.js, improving user interaction and responsiveness.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+      },
+      {
+        id: 2,
+        title: "Backend Developer - ORPP Kenya",
+        desc: "Designed and implemented secure and scalable backend services for ORPP's digital file management system, integrating OCR for document processing.",
+        className: "md:col-span-2",
+        thumbnail: "/exp2.svg",
+      },
+      {
+        id: 3,
+        title: "Freelance Full-Stack Developer",
+        desc: "Developed end-to-end web solutions for clients, including Minty (a banking app), StoreIt (a document management system), and BookWise (a library management system).",
+        className: "md:col-span-2",
+        thumbnail: "/exp3.svg",
+      },
+      {
+        id: 4,
+        title: "Lead Software Engineer - Startup Project",
+        desc: "Spearheaded the development of a fintech platform, optimizing financial transaction processes and enhancing user security.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+      }
+    ];
+    
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
+  
